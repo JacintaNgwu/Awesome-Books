f161f092a5762ae3a6f6042247392f8b8099c7a5
developertariq <dev.tariqul.islam@gmail.com>
2022-11-28 20:46:28 +0600

    Display all books in the collection

--- js/data.js
+++ js/data.js
@@ -1,22 +1,63 @@
 
+/*
 let bookList = [
   {title: 'Loreum ipsum',
   author: 'Testeroo Testyy'},
   {title: 'Second Book',
   author: 'Testeroo Testyy'}
 ];
+**/
 
-let book = {
+let bookList = [{
   Title: '',
   Author: ''
-}
+}];
+
 
 const addBookForm = document.getElementById('add_book_form');
+const bookListDiv = document.getElementById('book-list');
+
+function showBookList () {
+  document.getElementById('book-list').innerHTML = '';
+  
+  if (localStorage.getItem('bookList') !== null) {
+    bookList = JSON.parse(localStorage.getItem('bookList'));
+    bookList.forEach((book) => {
+
+      const titleDiv = document.createElement('div');
+      titleDiv.id = 'title-div';
+      titleDiv.innerText = book.Title;
+      document.getElementById('book-list').appendChild(titleDiv);
+
+      const authorDiv = document.createElement('div');
+      authorDiv.id = 'author-div';
+      authorDiv.innerText = book.Author;
+      document.getElementById('book-list').appendChild(authorDiv);
+
+      const removeBtn = document.createElement('button');
+      removeBtn.classList.add('remove-book');
+      removeBtn.innerText = 'Remove';
+      document.getElementById('book-list').appendChild(removeBtn);
+    });
+  }
+}
 
 
 document.getElementById('add_book_btn').addEventListener('click', () => {
+  const book = {
+    Title: '',
+    Author: ''
+  };
+
   book.Title = document.getElementById('title').value;
   book.Author = document.getElementById('author').value;
 
-  localStorage.setItem('book', JSON.stringify('book'));
+  bookList.push(book);
+
+  localStorage.setItem('bookList', JSON.stringify(bookList));
+  showBookList ();
+});
+
+window.addEventListener('load', () => {
+  showBookList ();
 });
